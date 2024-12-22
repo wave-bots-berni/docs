c577146c3cb5c7749eaea9a79d1f8c9b4738cbfe
Ubuntu <ubuntu@ov-b8cf54.infomaniak.ch>
2024-12-22 13:59:10 +0000

    docs update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,13 +22,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/palenight');
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/b-bots/docs/edit/main/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/b-bots/docs/edit/main/website/blog/',
+          editUrl: 'https://github.com/b-bots/docs/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
